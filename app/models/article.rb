1d0da1985aaa24f98f8859d788e103ede2f26bdf
mogh <mogh@example.com>
2016-04-28 19:25:27 -0400

    Added validations and scopes to Article

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,11 @@ class Article < ActiveRecord::Base
   # Relationships
   belongs_to :category
 
+  # Validations
+  validate_presence_of :title, :context
+
+  # Scopes
+  scope :alphabetical, -> { order("title") }
+  scope :active,       -> { where(active: true) }
+
 end
